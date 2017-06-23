8913542e7bcd851cccd63ccd94abaa0d5e9492eb
young <zxf.0810@163.com>
2017-06-23 18:03:25 +0800

    init program

---
+++ app/src/cart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cart.in/admin/');
+define('HTTP_CATALOG', 'http://cart.in/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cart.in/admin/');
+define('HTTPS_CATALOG', 'http://cart.in/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/cart/admin/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/cart/system/');
+define('DIR_IMAGE', '/usr/share/nginx/html/cart/image/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/cart/admin/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/cart/admin/view/template/');
+define('DIR_CONFIG', '/usr/share/nginx/html/cart/system/config/');
+define('DIR_CACHE', '/usr/share/nginx/html/cart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/cart/system/storage/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/cart/system/storage/logs/');
+define('DIR_MODIFICATION', '/usr/share/nginx/html/cart/system/storage/modification/');
+define('DIR_UPLOAD', '/usr/share/nginx/html/cart/system/storage/upload/');
+define('DIR_CATALOG', '/usr/share/nginx/html/cart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'cart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ app/src/cart/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cart.in/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cart.in/');
+
+// DIR
+define('DIR_APPLICATION', '/usr/share/nginx/html/cart/catalog/');
+define('DIR_SYSTEM', '/usr/share/nginx/html/cart/system/');
+define('DIR_IMAGE', '/usr/share/nginx/html/cart/image/');
+define('DIR_LANGUAGE', '/usr/share/nginx/html/cart/catalog/language/');
+define('DIR_TEMPLATE', '/usr/share/nginx/html/cart/catalog/view/theme/');
+define('DIR_CONFIG', '/usr/share/nginx/html/cart/system/config/');
+define('DIR_CACHE', '/usr/share/nginx/html/cart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/usr/share/nginx/html/cart/system/storage/download/');
+define('DIR_LOGS', '/usr/share/nginx/html/cart/system/storage/logs/');
+define('DIR_MODIFICATION', '/usr/share/nginx/html/cart/system/storage/modification/');
+define('DIR_UPLOAD', '/usr/share/nginx/html/cart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123456');
+define('DB_DATABASE', 'cart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
